c9739a0f864f30ec5e08957680e8aac660398771
Pranav Bokey <pbokey@gmail.com>
2019-11-20 13:16:20 -0500

    removed password

--- mongo/mongo_connection.py
+++ mongo/mongo_connection.py
@@ -3,7 +3,7 @@ from scraper import scraper
 from bson.objectid import ObjectId
 
 
-client = pymongo.MongoClient("mongodb://pranav_user:Pranav198!@cluster0-shard-00-00-rxhhi.mongodb.net:27017,cluster0-shard-00-01-rxhhi.mongodb.net:27017,cluster0-shard-00-02-rxhhi.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+client = pymongo.MongoClient("mongodb://pranav_user:@cluster0-shard-00-00-rxhhi.mongodb.net:27017,cluster0-shard-00-01-rxhhi.mongodb.net:27017,cluster0-shard-00-02-rxhhi.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 db = client['Football_Stats']
 collection = db['Jerry_Rice_Stats']
 print(collection)
